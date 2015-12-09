c6a0d104abca221c4e73b9995b8542dc0589f5aa
David Haemers <david_haemers@msn.com>
2015-12-09 14:52:52 +0100

    k

--- github/src/github/Hello.java
+++ github/src/github/Hello.java
@@ -2,7 +2,7 @@ package github;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, world, test 10.3");
+		System.out.println("Hello, sander, test 10.3");
 	}
 
 	/**
